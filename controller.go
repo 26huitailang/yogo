caec52ea103ddda76469dbd51375bba77f07d2b8
26huitailang <26huitailang@gmail.com>
2021-09-19 20:21:04 +0800

    update: custom context

--- controller.go
+++ controller.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/26huitailang/yogo/framework"
 )
 
 func FooControllerHandler(ctx *framework.Context) error {
-	return ctx.Json(http.StatusOK, map[string]interface{}{
-		"code": 0,
-	})
+	finish := make(chan struct{}, 1)
+	panicChan := make(chan interface{}, 1)
+
+	durationCtx, cancel := context.WithTimeout(ctx.BaseContext(), time.Duration(1*time.Second))
+	defer cancel()
+
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				panicChan <- p
+			}
+		}()
+		time.Sleep(6 * time.Second)
+		ctx.Json(200, "OK")
+		finish <- struct{}{}
+	}()
+	select {
+	case p := <-panicChan:
+		ctx.WriterMux().Lock()
+		defer ctx.WriterMux().Unlock()
+		log.Println(p)
+		ctx.Json(http.StatusInternalServerError, "panic")
+	case <-finish:
+		log.Println("finish")
+	case <-durationCtx.Done():
+		ctx.WriterMux().Lock()
+		defer ctx.WriterMux().Unlock()
+		ctx.Json(http.StatusInternalServerError, "time out")
+		ctx.SetHasTimeout()
+	}
+	return nil
 }
