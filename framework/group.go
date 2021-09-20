194706ec20dad7cc07ee0b275ea65d90e9b6883a
26huitailang <26huitailang@gmail.com>
2021-09-20 21:03:46 +0800

    feat: nested group

--- framework/group.go
+++ framework/group.go
@@ -5,36 +5,52 @@ type IGroup interface {
 	Post(string, ControllerHandler)
 	Put(string, ControllerHandler)
 	Delete(string, ControllerHandler)
+	Group(string) IGroup
 }
 
 type Group struct {
 	core   *Core
+	parent *Group
 	prefix string
 }
 
 func NewGroup(core *Core, prefix string) *Group {
 	return &Group{
 		core:   core,
+		parent: nil,
 		prefix: prefix,
 	}
 }
 
 func (g *Group) Get(uri string, handler ControllerHandler) {
-	uri = g.prefix + uri
+	uri = g.getAbsolutePrefix() + uri
 	g.core.Get(uri, handler)
 }
 
 func (g *Group) Post(uri string, handler ControllerHandler) {
-	uri = g.prefix + uri
+	uri = g.getAbsolutePrefix() + uri
 	g.core.Post(uri, handler)
 }
 
 func (g *Group) Put(uri string, handler ControllerHandler) {
-	uri = g.prefix + uri
+	uri = g.getAbsolutePrefix() + uri
 	g.core.Put(uri, handler)
 }
 
 func (g *Group) Delete(uri string, handler ControllerHandler) {
-	uri = g.prefix + uri
+	uri = g.getAbsolutePrefix() + uri
 	g.core.Delete(uri, handler)
+}
+
+func (g *Group) getAbsolutePrefix() string {
+	if g.parent == nil {
+		return g.prefix
+	}
+	return g.parent.getAbsolutePrefix() + g.prefix
+}
+
+func (g *Group) Group(uri string) IGroup {
+	cgroup := NewGroup(g.core, uri)
+	cgroup.parent = g
+	return cgroup
 }
