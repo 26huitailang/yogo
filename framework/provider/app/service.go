316c1256ae09419555ec857108a2c26951846631
26huitailang <26huitailang@gmail.com>
2021-10-31 20:18:36 +0800

    feat: app as a service

--- framework/provider/app/service.go
+++ framework/provider/app/service.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"errors"
 	"flag"
 	"github.com/26huitailang/yogo/framework"
 	"github.com/26huitailang/yogo/framework/util"
@@ -10,6 +11,34 @@ import (
 type YogoApp struct {
 	container  framework.Container
 	baseFolder string
+}
+
+func (y YogoApp) Version() string {
+	return "0.0.1"
+}
+
+func (y YogoApp) ConfigFolder() string {
+	return filepath.Join(y.BaseFolder(), "config")
+}
+
+func (y YogoApp) ProviderFolder() string {
+	return filepath.Join(y.BaseFolder(), "provider")
+}
+
+func (y YogoApp) MiddlewareFolder() string {
+	return filepath.Join(y.BaseFolder(), "middleware")
+}
+
+func (y YogoApp) CommandFolder() string {
+	return filepath.Join(y.BaseFolder(), "command")
+}
+
+func (y YogoApp) RuntimeFolder() string {
+	return filepath.Join(y.BaseFolder(), "runtime")
+}
+
+func (y YogoApp) TestFolder() string {
+	return filepath.Join(y.BaseFolder(), "test")
 }
 
 func (y YogoApp) BaseFolder() string {
@@ -32,4 +61,13 @@ func (y YogoApp) StorageFolder() string {
 
 func (y YogoApp) LogFolder() string {
 	return filepath.Join(y.BaseFolder(), "log")
+}
+
+func NewYogoApp(params ...interface{}) (interface{}, error) {
+	if len(params) != 2 {
+		return nil, errors.New("param error")
+	}
+	container := params[0].(framework.Container)
+	baseFolder := params[1].(string)
+	return &YogoApp{baseFolder: baseFolder, container: container}, nil
 }
