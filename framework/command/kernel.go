95821722488e77a6105fcbedf099ba862ded0f73
26huitailang <26huitailang@gmail.com>
2021-11-14 20:57:06 +0800

    :sparkles: rootCmd cron command

--- framework/command/kernel.go
+++ framework/command/kernel.go
@@ -5,4 +5,5 @@ import "github.com/26huitailang/yogo/framework/cobra"
 func AddKernelCommands(root *cobra.Command) {
 	root.AddCommand(DemoCommand)
 	root.AddCommand(initAppCommand())
+	root.AddCommand(initCronCommand())
 }
