54919694c6bb68d7e2a8be4d70f1bd7fb1d016ae
26huitailang <26huitailang@gmail.com>
2024-03-06 02:24:52 +0000

    chore: dev container

--- scripts/install-front-dev.sh
+++ scripts/install-front-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install nodejs -y
 node --version
-sudo npm install -g pnpm
+npm install -g pnpm
 pnpm --version
